133e1fe744c4e19ceb057f46c79cd3c8d55134b5
HannaGu <70803052+HannaGu@users.noreply.github.com>
2021-03-15 22:36:14 +0300

    Add files via upload

---
+++ Lab1/Lab01_textCalc/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Lab01_textCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.replace = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.get_by_indx = new System.Windows.Forms.Button();
+            this.length = new System.Windows.Forms.Button();
+            this.vowel = new System.Windows.Forms.Button();
+            this.consonant = new System.Windows.Forms.Button();
+            this.sentences = new System.Windows.Forms.Button();
+            this.words = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.equal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft New Tai Lue", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(32, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(673, 43);
+            this.textBox1.TabIndex = 0;
+            // 
+            // replace
+            // 
+            this.replace.Location = new System.Drawing.Point(33, 104);
+            this.replace.Name = "replace";
+            this.replace.Size = new System.Drawing.Size(90, 63);
+            this.replace.TabIndex = 1;
+            this.replace.Text = "replace";
+            this.replace.UseVisualStyleBackColor = true;
+            this.replace.Click += new System.EventHandler(this.replace_Click);
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(151, 104);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(90, 63);
+            this.delete.TabIndex = 2;
+            this.delete.Text = "delete";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // get_by_indx
+            // 
+            this.get_by_indx.Location = new System.Drawing.Point(266, 104);
+            this.get_by_indx.Name = "get_by_indx";
+            this.get_by_indx.Size = new System.Drawing.Size(97, 63);
+            this.get_by_indx.TabIndex = 3;
+            this.get_by_indx.Text = "get_by_indx";
+            this.get_by_indx.UseVisualStyleBackColor = true;
+            this.get_by_indx.Click += new System.EventHandler(this.get_by_indx_Click);
+            // 
+            // length
+            // 
+            this.length.Location = new System.Drawing.Point(616, 104);
+            this.length.Name = "length";
+            this.length.Size = new System.Drawing.Size(90, 63);
+            this.length.TabIndex = 4;
+            this.length.Text = "length";
+            this.length.UseVisualStyleBackColor = true;
+            this.length.Click += new System.EventHandler(this.length_Click);
+            // 
+            // vowel
+            // 
+            this.vowel.Location = new System.Drawing.Point(266, 187);
+            this.vowel.Name = "vowel";
+            this.vowel.Size = new System.Drawing.Size(97, 63);
+            this.vowel.TabIndex = 5;
+            this.vowel.Text = "vowel";
+            this.vowel.UseVisualStyleBackColor = true;
+            this.vowel.Click += new System.EventHandler(this.vowel_Click);
+            // 
+            // consonant
+            // 
+            this.consonant.Location = new System.Drawing.Point(387, 187);
+            this.consonant.Name = "consonant";
+            this.consonant.Size = new System.Drawing.Size(97, 63);
+            this.consonant.TabIndex = 6;
+            this.consonant.Text = "consonant";
+            this.consonant.UseVisualStyleBackColor = true;
+            this.consonant.Click += new System.EventHandler(this.consonant_Click);
+            // 
+            // sentences
+            // 
+            this.sentences.Location = new System.Drawing.Point(387, 104);
+            this.sentences.Name = "sentences";
+            this.sentences.Size = new System.Drawing.Size(90, 63);
+            this.sentences.TabIndex = 7;
+            this.sentences.Text = "sentences";
+            this.sentences.UseVisualStyleBackColor = true;
+            this.sentences.Click += new System.EventHandler(this.sentences_Click);
+            // 
+            // words
+            // 
+            this.words.Location = new System.Drawing.Point(498, 104);
+            this.words.Name = "words";
+            this.words.Size = new System.Drawing.Size(90, 63);
+            this.words.TabIndex = 8;
+            this.words.Text = "words";
+            this.words.UseVisualStyleBackColor = true;
+            this.words.Click += new System.EventHandler(this.words_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(151, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 63);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "C";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // equal
+            // 
+            this.equal.Location = new System.Drawing.Point(33, 187);
+            this.equal.Name = "equal";
+            this.equal.Size = new System.Drawing.Size(90, 63);
+            this.equal.TabIndex = 10;
+            this.equal.Text = "=";
+            this.equal.UseVisualStyleBackColor = true;
+            this.equal.Click += new System.EventHandler(this.equal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 279);
+            this.Controls.Add(this.equal);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.words);
+            this.Controls.Add(this.sentences);
+            this.Controls.Add(this.consonant);
+            this.Controls.Add(this.vowel);
+            this.Controls.Add(this.length);
+            this.Controls.Add(this.get_by_indx);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.replace);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button replace;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Button get_by_indx;
+        private System.Windows.Forms.Button length;
+        private System.Windows.Forms.Button vowel;
+        private System.Windows.Forms.Button consonant;
+        private System.Windows.Forms.Button sentences;
+        private System.Windows.Forms.Button words;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button equal;
+    }
+}
+
---
+++ Lab1/Lab01_textCalc/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab01_textCalc
+{
+    public partial class Form1 : Form
+    {
+        public string n1;
+        public string operation;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            n1 = textBox1.Text;
+            Button B = (Button)sender;
+            operation = B.Text;
+            textBox1.Text = "";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+        private void get_by_indx_Click(object sender, EventArgs e)
+        {
+            n1 = textBox1.Text;   
+            Button B = (Button)sender;
+            operation = B.Text;
+            textBox1.Text = "";
+        }
+        private void length_Click(object sender, EventArgs e)
+        {
+            n1 = textBox1.Text;
+            textBox1.Text = Convert.ToString(n1.Length);
+        }
+        private void vowel_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            string vow = "AaEeIiOoUuYyАаУуОоЫыИиЭэЯяЮюЁёЕе";
+            n1 = textBox1.Text;
+            foreach(char c in n1)
+            {
+                if (vow.IndexOf(c) != -1)
+                    count++;
+            }
+            textBox1.Text=Convert.ToString(count);   
+        }
+        private void consonant_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            string vow = "AaEeIiOoUuYyАаУуОоЫыИиЭэЯяЮюЁёЕе";
+            string punct = " /()+-=* !?.,-;:0123456789";
+            n1 = textBox1.Text;
+            foreach (char c in n1)
+            {
+                if (vow.IndexOf(c) == -1&&punct.IndexOf(c)==-1)
+                    count++;
+            }
+            textBox1.Text = Convert.ToString(count);
+        }
+
+        private void sentences_Click(object sender, EventArgs e)
+        {
+            string punct = ".!?";
+            int count = 0;
+            n1 = textBox1.Text;
+            foreach (char c in n1)
+            {
+                if (punct.IndexOf(c) != -1)
+                    count++;
+            }
+            textBox1.Text = Convert.ToString(count);
+        }
+        private void replace_Click(object sender, EventArgs e)
+        {
+            n1 = textBox1.Text;
+            Button B = (Button)sender;
+            operation = B.Text;
+            textBox1.Text = "";
+        }
+
+        private void words_Click(object sender, EventArgs e)
+        {
+            int count = 1;
+            string punct = " ,.!?";
+            n1 = textBox1.Text;
+            for (int i=0;i<n1.Length;i++)
+            {
+                
+                if (n1[i]==' '&&punct.IndexOf(n1[i+1])==-1)
+                    count++;
+
+            }
+            textBox1.Text = Convert.ToString(count);
+        }
+        private void equal_Click(object sender, EventArgs e)
+        {
+            string result="", n2;
+            char symb;
+            int indxN2;
+
+            switch(operation)
+            {
+                case "delete": {
+                        n2 = textBox1.Text;
+                        indxN2=n1.IndexOf(n2);
+
+                        if(indxN2 ==-1)
+                            throw new Exception("Подстрока не найдена");
+                       
+                        result = n1.Remove(indxN2, n2.Length);
+                        break;
+                    }
+                case "get_by_indx": {
+                        string str= "0123456789";
+                        foreach(char c in textBox1.Text)
+                        { 
+                            if (str.IndexOf(c) == -1)
+                                throw new Exception("Индекс должен быть числом");
+                        }
+                        indxN2 = Convert.ToInt32(textBox1.Text);
+                        if (indxN2 > n1.Length)
+                            throw new Exception("Индекс превышает длину строки");
+                        symb = n1[indxN2];
+                        result = Convert.ToString(symb);
+                        break;
+                    }
+                case "replace": {
+                        bool sharp = false;
+                        n2 = textBox1.Text;
+                        string first="", second="";
+                        foreach(char c in n2)
+                        {
+                            if (c == '#') { sharp = true; continue; }
+                            if(!sharp) first+=c;
+                            else second += c;
+                        }
+                        if (n1.IndexOf(first) == -1)
+                            throw new Exception("Подстрока для замены не найдена");
+                        result=n1.Replace(first, second);
+                        break;
+                    }
+                
+            }
+            textBox1.Text = result;
+            operation = "";
+            n1 = "";
+        }
+
+       
+    }
+}
