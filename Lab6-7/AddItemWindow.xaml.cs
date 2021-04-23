4b6c862c2c7002d1039a7e983b66f620959c94fe
HannaGu <70803052+HannaGu@users.noreply.github.com>
2021-04-23 23:48:31 +0300

    Add files via upload

---
+++ Lab6-7/AddItemWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Resources;
+using System.Windows.Shapes;
+
+namespace Lab6_7
+{
+    /// <summary>
+    /// Логика взаимодействия для AddItemWindow.xaml
+    /// </summary>
+    public partial class AddItemWindow : Window
+    {
+        public AddItemWindow()
+        {
+            InitializeComponent();
+            this.Cursor = Cursors.None;
+            StreamResourceInfo sri = Application.GetResourceStream(
+             new Uri("Cursor/1.cur", UriKind.Relative));
+            Cursor customCursor = new Cursor(sri.Stream);
+            this.Cursor = customCursor;
+        }
+        private Command addCommand;
+        private Command closeCommand;
+        public ICommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                  (addCommand = new Command(obj =>
+                  {
+                  try
+                  {
+                      Product pr = new Product();
+                          pr.Id = Convert.ToInt32(idTB.Text);
+                          pr.Name = nameTB.Text;
+                          pr.Description = descriptionTB.Text;
+                          pr.Quantity = Convert.ToInt32(quantityTB.Text);
+                          pr.Price = Convert.ToDouble(priceTB.Text);
+                          pr.ImagePath = imgPathTB.Text;
+                          pr.Color = colorTB.Text;
+                          Serializer.Serializer.AddNode(pr, "D:\\СЕМ 4\\ООТП_2\\Lab6-7\\Lab6-7\\bin\\Debug\\Items.xml");
+                          MainControl.products.Add(pr);
+                      }
+                      catch
+                      {
+                          MessageBox.Show("Введены некорретные данные!");
+                      }
+                  }));
+            }
+        }
+
+        public ICommand CloseCommand
+        {
+            get
+            {
+                return closeCommand ??
+                  (closeCommand = new Command(obj =>
+                  {
+                      this.Close();
+                  }));
+            }
+        }
+    }
+}
---
+++ Lab6-7/DeleteWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+using System.Windows.Resources;
+
+namespace Lab6_7
+{
+    /// <summary>
+    /// Логика взаимодействия для DeleteWindow.xaml
+    /// </summary>
+    public partial class DeleteWindow : Window
+    {
+        public DeleteWindow()
+        {
+            InitializeComponent();
+            this.Cursor = Cursors.None;
+            StreamResourceInfo sri = Application.GetResourceStream(
+             new Uri("Cursor/1.cur", UriKind.Relative));
+            Cursor customCursor = new Cursor(sri.Stream);
+            this.Cursor = customCursor;
+        }
+        private Command deleteCommand;
+   
+        public ICommand DeleteCommand
+        {
+            get
+            {
+                return deleteCommand ??
+                  (deleteCommand = new Command(obj =>
+                  {
+                  try
+                  {
+                      char[] separators = { ' ', ','};
+                      string[] idStr = idTextBox.Text.Split(separators);
+                      string[] resultarr=idStr.Where(x => Regex.IsMatch(x, @"^\d+$")).ToArray();
+                      foreach (string s in resultarr)
+                      {
+                        int id = Convert.ToInt32(s);
+                        Serializer.Serializer.DeleteNode("D:\\СЕМ 4\\ООТП_2\\Lab6-7\\Lab6-7\\bin\\Debug\\Items.xml", id);
+                      }
+                  }
+                      catch
+                      {
+                          MessageBox.Show("Введены некорретные данные!");
+                      }
+                  }));  
+            }
+        }
+
+    }
+}
